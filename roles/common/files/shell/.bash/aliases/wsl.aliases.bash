fe0d12fc69c8fa21267ce9696110e5719dc936bb
Bingzhang Dai <bingzhangdai@outlook.com>
2021-03-21 17:19:09 +0800

    update alias

--- roles/common/files/shell/.bash/aliases/wsl.aliases.bash
+++ roles/common/files/shell/.bash/aliases/wsl.aliases.bash
@@ -15,15 +15,26 @@ if [[ -n "$(_get_wsl_version)" ]]; then
 
     alias cmd='cmd.exe'
 
+    function _get_win_path() {
+        if [[ $# -eq 0 ]]; then
+            printf -- '.'
+        else
+            local _winpath="$(wslpath -w $* 2> /dev/null || echo $*)"
+            printf -- '%s' "$_winpath"
+        fi
+    }
+
     # https://github.com/microsoft/WSL/issues/6565
     # explorer.exe always return 1 regardless the result
     function explorer() {
-        if [[ $# -eq 0 ]]; then
-            powershell.exe start explorer.exe .
-        else
-            local _winpath="$(wslpath -w $* 2> /dev/null || echo $*)"
-            [[ -z "$_winpath" ]] && _winpath='.'
-            powershell.exe start explorer.exe "$_winpath"
-        fi
+        local _winpath="$(_get_win_path $*)"
+        powershell.exe start explorer.exe "$_winpath"
     }
+
+    if command -v code > /dev/null; then
+        function code() {
+            local _winpath="$(_get_win_path $*)"
+            cmd.exe /c code "$_winpath"
+        }
+    fi
 fi
