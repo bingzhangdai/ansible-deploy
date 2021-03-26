3300bfff5c5ce3f7db184eb3961e860083fe4f44
Bingzhang Dai <bingzhangdai@outlook.com>
2021-03-26 12:51:04 +0800

    add pip ssh completion

--- roles/common/files/shell/.bash/completions/ssh.completion.bash
+++ roles/common/files/shell/.bash/completions/ssh.completion.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Bash completion support for ssh.
+# https://github.com/Bash-it/bash-it/blob/master/completion/available/ssh.completion.bash
 
 export COMP_WORDBREAKS=${COMP_WORDBREAKS/\:/}
 
@@ -11,11 +12,16 @@ _sshcomplete() {
         local OPTIONS=" -- ${CURRENT_PROMPT}"
     fi
 
-
-    # parse all defined hosts from .ssh/config
-    if [ -r "$HOME/.ssh/config" ]; then
-        COMPREPLY=($(compgen -W "$(grep ^Host "$HOME/.ssh/config" | awk '{for (i=2; i<=NF; i++) print $i}' )" ${OPTIONS}) )
-    fi
+    # parse all defined hosts from .ssh/config and files included there
+    for fl in "$HOME/.ssh/config" \
+        $(grep "^\s*Include" "$HOME/.ssh/config" |
+            awk '{for (i=2; i<=NF; i++) print $i}' |
+            sed -Ee "s|^([^/~])|$HOME/.ssh/\1|" -e "s|^~/|$HOME/|")
+    do
+        if [ -r "$fl" ]; then
+            COMPREPLY=( ${COMPREPLY[@]} $(compgen -W "$(grep -i ^Host "$fl" |grep -v '[*!]' | awk '{for (i=2; i<=NF; i++) print $i}' )" ${OPTIONS}) )
+        fi
+    done
 
     # parse all hosts found in .ssh/known_hosts
     if [ -r "$HOME/.ssh/known_hosts" ]; then
@@ -30,4 +36,4 @@ _sshcomplete() {
     fi
 }
 
-complete -o default -o nospace -F _sshcomplete ssh scp
+complete -o default -o nospace -F _sshcomplete ssh scp slogin sftp
